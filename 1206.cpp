e4821f74bc163b064fd7a1e61d0b9b437bab0321
liamlin <liam_lin@17life.com>
2024-12-13 19:30:28 +0800

    feat:1206p1 1206p2 section

--- 1206.cpp
+++ 1206.cpp
@@ -7,13 +7,13 @@ using namespace std;
 //這題的核心，別想太多，這題要用到有向圖跟拓撲排序法，基本上我卡在．如果同時多個indegree為0
 class Solution {
 public:
-    std::vector<std::vector<std::pair<char,int>>> area ; 
-    int yLimit = area.size();
-    int xLimit = area[0].size();
+    std::vector<std::vector<std::pair<char,int>>> area = std::vector<std::vector<std::pair<char,int>>>(); 
+    int yLimit = 0;
+    int xLimit = 0 ;
     std::vector<std::pair<int,int>> directions = std::vector<std::pair<int,int>>{
-        {0,1},
-        {1,0},
         {0,-1},
+        {1,0},
+        {0,1},
         {-1,0}
     };
     bool in = true;
@@ -21,57 +21,51 @@ public:
     int backtracking(int& guardX,int& guardY,std::pair<int,int> nowDirection){
         int newX = guardX+nowDirection.first;
         int newY = guardY+nowDirection.second;
-        if(newX==xLimit || newY==yLimit||area[newY][newX].first=='#'){
+        if(newX<0||newY<0||newX>=xLimit || newY>=yLimit||area[newY][newX].first=='#'){
+            if(newX<0||newY<0||newX>=xLimit || newY>=yLimit) in = false;
             return 0;
         }
-        area[newY][newX].second = 0;
-        return area[newY][newX].second+backtracking(newX,newY,nowDirection);
+        guardX = newX;
+        guardY = newY;
+        int newArea =  area[guardY][guardX].second;
+        area[guardY][guardX].second = 0;
+        return newArea+backtracking(guardX,guardY,nowDirection);
     }
-    int movingOut(int guardX,int guardY){
-        int answer = 0;
-        while(guardX!=xLimit && guardY==yLimit){
-            for(auto direction : directions){
-                answer+=backtracking(guardX,guardY,direction);
-            }
+    int movingOut(int& guardX,int& guardY){
+        int answer = 1;
+        int cycle = 0;
+        area[guardY][guardX].second = 0;
+        while(in){
+            // cout<<guardX<<guardY<<endl;
+            // cout<<cycle%4<<endl;
+            answer+=backtracking(guardX,guardY,directions[cycle++%4]);
         }
-        return  
+        return answer;
     }
 };
 
 int main(){
-    std::ifstream inputFile("rule.txt");
+    std::ifstream inputFile("input.txt");
     std::string line ;
     Solution solution  = Solution();
-    std::vector<std::vector<int>> allIntLine = std::vector<std::vector<int>>(); 
+    int anwer= 0;
+    int guardX = 0;
+    int guardY = 0;
+    int nowY = 0;
     while (std::getline(inputFile, line)) { 
-        allIntLine.emplace_back(solution.split(line,'|'));
+        std::vector<std::pair<char,int>> temp = std::vector<std::pair<char,int>>();
+        for(int nowX = 0 ; nowX<line.size();nowX++){
+            temp.emplace_back(line[nowX],1);
+            if(line[nowX]=='^'){
+                guardX = nowX;
+                guardY = nowY;
+            }
+        }
+        solution.area.emplace_back(temp);
+        nowY++;
     }  
-    std::unordered_map<int,std::unordered_set<int>> after;
-    std::unordered_map<int,std::unordered_set<int>> before;
-    int index = 0;
-    for(auto intLine : allIntLine){
-        int beforeNumber = intLine[0];
-        int afterNumber = intLine[1];
-        if(!after.contains(beforeNumber)) after[beforeNumber] = std::unordered_set<int>();
-        after[beforeNumber].emplace(afterNumber);
-        if(!before.contains(afterNumber)) before[afterNumber] = std::unordered_set<int>();
-        before[afterNumber].emplace(beforeNumber);
-    }
-    
-    std::unordered_map<int,std::vector<int>> graph;
-    for(auto intLine : allIntLine){
-        int beforeNumber = intLine[0];
-        int afterNumber = intLine[1];
-        if(!graph.contains(beforeNumber)) graph[beforeNumber] =  std::vector<int>();
-        graph[beforeNumber].emplace_back(afterNumber);
-    }
-
-    std::ifstream inputFile2("input.txt");
-    while (std::getline(inputFile2, line)) { 
-       solution.sumMiddleNumberP2(solution.split(line,','),graph);
-    }   
-    inputFile.close();   
-    cout<<solution.answer<<endl; 
+    solution.xLimit =  solution.area[0].size();
+    solution.yLimit = solution.area.size();
+    cout<<solution.movingOut(guardX,guardY)<<endl;
     return 0;
-    
 };
